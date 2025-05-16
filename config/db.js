4b72e3141ff118081b5ea3b48cff29cf2f5a8a71
Hani78789 <hanisha.abubakkarsithick123@gmail.com>
2025-05-16 10:18:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://hanishaa433:hanisha%402001@cluster0.ndvmzbw.mongodb.net/mearnapp?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://hanishaa433:hanisha%402001@cluster0.ndvmzbw.mongodb.net/mearnapp?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 let db;
@@ -8,17 +8,17 @@ let db;
 const connectToDb = async () => {
   try {
     await client.connect();
-    db = client.db('mearnapp'); // <- match this with your URI DB
-    console.log(" Connected to MongoDB");
+    db = client.db('node_assignment');
+    console.log("Connected to MongoDB");
   } catch (err) {
-    console.error(" Error connecting to MongoDB:", err);
+    console.error("Error connecting to MongoDB:", err);
     throw err;
   }
 };
 
 const getDb = () => {
   if (!db) {
-    throw new Error(' Database not initialized');
+    throw new Error('Database not initialized');
   }
   return db;
 };
